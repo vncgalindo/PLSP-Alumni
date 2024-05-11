352ea82d130e4cb908b072c06becd7e79597a562
vncgalindo <vncgalindo@gmail.com>
2024-05-11 20:50:26 +0800

    Connection for database

---
+++ connect/connect.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+date_default_timezone_set('Asia/Manila');
+$date_current = date('Y-m-d H:i:s');
+?>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "alumniportal";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    echo "<br>Failed to connect to the network";
+} 
+
+?>

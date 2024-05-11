4b9e0738a8e477e5f93738440c65c39d222844a5
vncgalindo <vncgalindo@gmail.com>
2024-05-11 21:26:19 +0800

    Add files via upload

---
+++ adminpage/topnav.php
@@ -0,0 +1,22 @@
+<ul class="navbar-nav">
+    <li class="nav-item">
+        <a href="#" class="nav-link" style="padding-left:10px">
+            Home
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="#" class="nav-link" style="padding-left:10px">
+            Events
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="#" class="nav-link" style="padding-left:10px">
+            Gallery 
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="#" class="nav-link" style="padding-left:10px">
+            About Us 
+        </a>
+    </li>
+</ul>

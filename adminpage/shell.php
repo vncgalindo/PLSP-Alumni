379d871a62065da1669a51ca16b05978184da190
vncgalindo <vncgalindo@gmail.com>
2024-05-11 21:20:08 +0800

    Website shell

---
+++ adminpage/shell.php
@@ -0,0 +1,113 @@
+<?php
+ob_start();
+include './connect/connect.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>PLSP Alumni Portal</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="./plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="./dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="./plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+</head>
+
+
+<body class="hold-transition layout-top-nav">
+  <div class="wrapper">
+    <nav class="main-header navbar navbar-expand-md navbar-dark navbar-navy">
+      <div class="container">
+       
+        <button class="navbar-toggler order-1" style="border-width: 0px" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse order-2" id="navbarCollapse">
+          <!-- Left navbar links -->
+          <?php
+          include "topnav.php";
+          ?>
+
+          <!-- SEARCH FORM -->
+        </div>
+
+        <!-- Right navbar links -->
+        <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="./shell.php?page=login">
+              Sign In
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Main content -->
+      <div class="content">
+        <div class="container">
+        <?php
+        if($_GET['page']=='login')
+        {
+        include "./userlog/userlog.php";
+        }
+        else if($_GET['page']=='register')
+        {
+        include "./userlog/register.php";
+        }
+        else if($_GET['page']=='forgot')
+        {
+        include "./userlog/forgot.php";
+        }
+        else if($_GET['page']=='change')
+        {
+        include "./userlog/change.php";
+        }
+        ?> 
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    <footer class="main-footer">
+      <!-- BOTTOM FOOTER LEFT SIDE -->
+      LOGO
+      <strong>PAMANTASAN NG LUNGSOD NG SAN PABLO</strong>
+      ALUMNI ASSOCIATION
+
+
+      <!-- BOTTOM FOOTER RIGHT SIDE -->
+      <div class="float-right d-none d-sm-inline-block">
+        ©<b>2024</b> PAMANTASAN NG LUNSOD NG SAN PABLO
+      </div>
+    </footer>
+
+  </div>
+<!-- ./wrapper -->
+</body>
+
+<!-- jQuery -->
+<script src="./plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="./plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="./plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="./plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="./dist/js/adminlte.js"></script>
+</html>
